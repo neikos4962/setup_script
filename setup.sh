fe4b9420f7f6835b8d7b7be122c811ac09ac70b1
Ҝ卄卂丂ㄥ卂几卂(NeiKos496) <alexandrusemina8@gmail.com>
2026-09-16 02:10:47 +0300

    Update vendor signing keys repository branch to main

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ declare -A REPOS=(
     ["packages/apps/ParanoidGlyphPhone2"]="https://github.com/Pong-Development/packages_apps_ParanoidGlyph.git|17"
     ["packages/apps/GlyphAdapter"]="https://github.com/Pong-Development/packages_apps_GlyphAdapter.git|16"
     ["hardware/dolby"]="https://github.com/Pong-Development/hardware_dolby.git|16"
-    ["vendor/lineage/signing/keys"]="https://github.com/neikos4962/vendor_lineage_signing_keys.git"
+    ["vendor/lineage/signing/keys"]="https://github.com/neikos4962/vendor_lineage_signing_keys.git|main"
 )
 
 # Function to clone a repository with error handling
